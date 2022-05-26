17da23c2f79504415bb8cefa9535fd0b6ca202e7
ddeleon267 <dominique.a.deleon@gmail.com>
2022-05-26 10:13:26 -0400

    commit tree e810ae973679b54d0670397ecdb3abcb241c6cca

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+    console.log(input)
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+
+    input.addEventListener('click', clickAlert);
 }
+
+addingEventListener()
